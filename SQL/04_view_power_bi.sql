730415629c13fd54c826ccf9d065852dfaab25f1
rodrigo-limaa <rodrigdelima@gmail.com>
2026-02-04 19:20:47 -0300

    Enhance vw_vendas_comercial view with clearer aliases
    Refactor SQL view to improve readability and structure.

--- SQL/04_view_power_bi.sql
+++ SQL/04_view_power_bi.sql
@@ -1,38 +1,42 @@
-----------------------------------------------------
-Projeto: Análise de Vendas
-Arquivo: 04_view_power_bi.sql
-----------------------------------------------------
-
-Objetivo:
-Criar uma VIEW consolidada com os dados de vendas,
-produtos, categorias e localização, servindo como
-fonte única para análises e dashboards no Power BI.
-----------------------------------------------------
+------------------------------------------------------------
+-- Projeto: Análise de Vendas
+-- Arquivo: 04_view_power_bi.sql
+-- Objetivo:
+-- Criar uma view consolidada como fonte única de dados
+-- para análises SQL e dashboards no Power BI.
+------------------------------------------------------------
 
 CREATE OR REPLACE VIEW vw_vendas_comercial AS
 SELECT
     -- Identificadores
-    p.id_pedido,
-    p.data_venda,
+
+    p.id_pedido            AS pedido_id,
+    p.data_venda           AS data_pedido,
 
     -- Produto
-    pr.id_produto,
-    pr.nome_produto,
-    c.nome AS nome_categoria,
+    
+    pr.id_produto          AS produto_id,
+    pr.nome_produto        AS produto,
+    c.nome                 AS categoria,
 
-    -- Loja e Localização
-    l.id_loja,
-    l.loja AS nome_loja,
+    
+    -- Loja e localização
+    l.id_loja              AS loja_id,
+    l.loja                 AS loja,
     lc.cidade,
     lc.estado,
     lc.regiao,
+    
 
     -- Métricas base
-    p.qtd_vendida,
-    p.preco_unit,
-    p.custo_unit,
+    p.qtd_vendida          AS quantidade,
+    p.preco_unit           AS preco_unitario,
+    p.custo_unit           AS custo_unitario,
 
+
+    
     -- Métricas calculadas
+    
     (p.qtd_vendida * p.preco_unit) AS faturamento,
     (p.qtd_vendida * (p.preco_unit - p.custo_unit)) AS lucro
 
