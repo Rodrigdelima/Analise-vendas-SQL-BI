aab19f919bf3f8d376987449e0cd21063c65403a
rodrigo-limaa <rodrigdelima@gmail.com>
2026-02-04 22:09:12 -0300

    Refactor vw_vendas_comercial view and add checks
    Updated the view definition for vw_vendas_comercial to improve clarity and added queries to visualize the view and check for NULL values.

--- SQL/04_view_power_bi.sql
+++ SQL/04_view_power_bi.sql
@@ -6,46 +6,50 @@
 -- para análises SQL e dashboards no Power BI.
 ------------------------------------------------------------
 
-CREATE OR REPLACE VIEW vw_vendas_comercial AS
+CREATE VIEW vw_vendas_comercial AS
 SELECT
-    -- Identificadores
-
-    p.id_pedido            AS pedido_id,
-    p.data_venda           AS data_pedido,
-
-    -- Produto
     
-    pr.id_produto          AS produto_id,
-    pr.nome_produto        AS produto,
-    c.nome                 AS categoria,
+    p.id_pedido  AS pedido_id,
+    p.data_venda AS data_pedido,
 
-    
-    -- Loja e localização
-    l.id_loja              AS loja_id,
-    l.loja                 AS loja,
+    pr.id_produto   AS produto_id,
+    pr.nome_produto AS produto,
+    c.nome          AS categoria,
+
+    l.id_loja AS loja_id,
+    l.loja    AS loja,
     lc.cidade,
     lc.estado,
     lc.regiao,
-    
 
-    -- Métricas base
-    p.qtd_vendida          AS quantidade,
-    p.preco_unit           AS preco_unitario,
-    p.custo_unit           AS custo_unitario,
+    p.qtd_vendida AS quantidade,
+    p.preco_unit  AS preco_unitario,
+    p.custo_unit  AS custo_unitario,
 
-
-    
-    -- Métricas calculadas
-    
+  
     (p.qtd_vendida * p.preco_unit) AS faturamento,
     (p.qtd_vendida * (p.preco_unit - p.custo_unit)) AS lucro
-
 FROM pedidos p
-JOIN produtos pr
-    ON p.id_produto = pr.id_produto
-JOIN categorias c
-    ON pr.id_categoria = c.id
-JOIN lojas l
-    ON p.id_loja = l.id_loja
-JOIN locais lc
-    ON l.id_local = lc.id_local;
+JOIN produtos pr  ON p.id_produto = pr.id_produto
+JOIN categorias c ON pr.id_categoria = c.id
+
+
+
+
+-- visualizacao da view --
+SELECT *
+FROM vw_vendas_comercial
+LIMIT 10;
+
+
+-- verificar se existem valores NULL--
+SELECT
+    COUNT(*) AS total_linhas,
+    COUNT(p.qtd_vendida) AS qtd_vendida_preenchido,
+    COUNT(p.preco_unit) AS preco_preenchido,
+    COUNT(p.custo_unit) AS custo_preenchido
+FROM pedidos p;
+
+    
+JOIN lojas l      ON p.id_loja = l.id_loja
+JOIN locais lc    ON l.id_local = lc.id_local;
